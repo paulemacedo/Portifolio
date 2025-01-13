9fb6b0c04595ac6de4160ce143a1734a07ec21a2
Paule Macedo <paulemacedo@outlook.com>
2025-01-13 00:29:05 -0300

    fix: update Open Graph image path and change button to link for CV download

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,12 +80,15 @@ const Hero = () => {
                     transition={{ duration: 1, delay: 1.5 }}
                     className="flex flex-row items-center gap-6 my-4 md:mb-0"
                     >   
-                        <motion.button whileHover={{ scale: 1.05, boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.3)' }} 
-                            className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
-                                         border-purple-400 rounded-xl"  
+                        <motion.a 
+                            whileHover={{ scale: 1.05, boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.3)' }} 
+                            className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border border-purple-400 rounded-xl"  
+                            href="/CV.pdf"
+                            target="_blank"
+                            rel="noopener noreferrer"
                         >
                         Download CV
-                        </motion.button>
+                        </motion.a>
 
                         <div className='flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20'>
                             <motion.a whileHover={{ scale: 1.2 }} href="https://github.com/paulemacedo">
