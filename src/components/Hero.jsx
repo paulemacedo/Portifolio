8facb294f46c415d634b614b7d437b6337798a68
Paule Macedo <paulemacedo@outlook.com>
2025-01-18 21:39:10 -0300

    feat: refactor Hero component for improved structure and readability

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,107 +46,98 @@ const Hero = () => {
                 viewport={{ once: true }}
                 transition={{ duration: 1 }}
                 >   
-                    <TypeAnimation
-                        sequence={[                        
-                            t('hero.software_engineer'),
-                            1000,
-                            t('hero.cybersecurity'),
-                            1000
-                        ]}
-                        repeat={Infinity}
-                        speed={50}
-                        className="font-bold text-xl md:text-5xl text-gray-400 italic- mb-4"
+                <TypeAnimation
+                    sequence={[                        
+                    t('hero.software_engineer'),
+                    1000,
+                    t('hero.cybersecurity'),
+                    1000
+                    ]}
+                    repeat={Infinity}
+                    speed={50}
+                    className="font-bold text-xl md:text-5xl text-gray-400 italic mb-4"
+                />
 
-                    />
+                <motion.p
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1}}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1, delay: 0.5 }}
+                    className='text-gray-200 md:text-5xl tracking-tight mb-4'
+                >
+                    {t('hero.greeting')} <br /> 
+                    <span className='text-purple-500'>PAULE MACEDO</span>
+                </motion.p>
 
-                    <motion.p
-                        initial={{ opacity: 0 }}
-                        whileInView={{ opacity: 1}}
-                        viewport={{ once: true }}
-                        transition={{ duration: 1, delay: 0.5 }}
-                        className='text-gray-200 md:text-5xl tracking-tight mb-4'
-                    >
-                        {t('hero.greeting')} <br /> 
-                        <span className='text-purple-500'>PAULE MACEDO</span>
+                <motion.p
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1}}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1, delay: 0.5 }}
+                    className='text-gray-300 max-w-[300px] md:text-2xl text-lg mb-6 md:max-w-[500px]'
+                >
+                    {t('hero.description')}                        
+                </motion.p>
 
-                    </motion.p>
-
-                    <motion.p
-                        initial={{ opacity: 0 }}
-                        whileInView={{ opacity: 1}}
-                        viewport={{ once: true }}
-                        transition={{ duration: 1, delay: 0.5 }}
-                        className='text-gray-300 max-w-[300px] md:text-2xl text-lg mb-6 md:max-w-[500px]'
-                    >
-                        {t('hero.description')}                        
-
-                    </motion.p>
-                    
-
-                    <motion.div
+                <motion.div
                     initial={{ opacity: 0, y: 50 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     viewport={{ once: true }}
                     transition={{ duration: 1, delay: 1.5 }}
-                    className="flex flex-row items-center gap-6 my-4 md:mb-0"
-                    >   
-                            <motion.a 
-                                whileHover={{ scale: 1.05, boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.3)' }} 
-                                className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border border-purple-400 rounded-xl"  
-                                onClick={openModal}
-                            >
-                                Download CV
-                            </motion.a>
-                            <Modal
-                                isOpen={modalIsOpen}
-                                onRequestClose={closeModal}
-                                contentLabel="Select CV"
-                                className="bg-purple-900 border border-purple-400 p-4 rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] md:w-[400px] z-50 backdrop-blur-md bg-opacity-20"
-                                overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-40 "
+                    className="flex flex-col items-start md:flex-row gap-6 my-4 md:mb-0"
+                >   
+                    <motion.a 
+                    whileHover={{ scale: 1.05, boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.3)' }} 
+                    className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border border-purple-400 rounded-xl"  
+                    onClick={openModal}
+                    >
+                    Download CV
+                    </motion.a>
+                    <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Select CV"
+                    className="bg-purple-900 border border-purple-400 p-4 rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] md:w-[400px] z-50 backdrop-blur-md bg-opacity-20"
+                    overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-40 "
+                    >
+                    <h2 className='text-purple-700 pb-4 text-4xl'>
+                        {t('hero.select_cv_language')}
+                    </h2>
+                    <div className='flex flex-col gap-4 items-start text-xl text-white'>
+                        <button 
+                        onClick={() => window.location.href = '/Paulo Macedo - CV - PT-BR.pdf'}
+                        >    
+                        {t('hero.brazilian_portuguese')}
+                        </button>
+                        <button onClick={() => window.location.href = '#'}>
+                        {t('hero.english')}
+                        </button>
+                    </div>
+                    </Modal>
 
-                                // flex flex-col items-start gap-4 my-4 md:mb-0
-                            >
-                                <h2 className='text-purple-700 pb-4 text-4xl'>
-                                  {t('hero.select_cv_language')}
-                                </h2>
-                                <div className='flex flex-col gap-4 items-start text-xl text-white'>
-                                    <button 
-                                    onClick={() => window.location.href = '/Paulo Macedo - CV - PT-BR.pdf'}
-                                    >    
-                                      {t('hero.brazilian_portuguese')}
-                                  </button>
-                                    <button onClick={() => window.location.href = '#'}>
-                                        {t('hero.english')}
-                                    </button>
-                                    {/* <button onClick={closeModal}>Close</button> */}
-                                </div>
-                            </Modal>
+                    <div className='flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20'>
+                    <motion.a whileHover={{ scale: 1.2 }} href="https://github.com/paulemacedo">
+                        <AiOutlineGithub />
+                    </motion.a>
 
-                        <div className='flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20'>
-                            <motion.a whileHover={{ scale: 1.2 }} href="https://github.com/paulemacedo">
-                                <AiOutlineGithub />
-                            </motion.a>
+                    <motion.a whileHover={{ scale: 1.2 }} href="https://www.linkedin.com/in/Paulemacedo/">
+                        <AiOutlineLinkedin />
+                    </motion.a>
 
-                            <motion.a whileHover={{ scale: 1.2 }} href="https://www.linkedin.com/in/Paulemacedo/">
-                                <AiOutlineLinkedin />
-                            </motion.a>
-
-                            <motion.a whileHover={{ scale: 1.2 }} href="https://tryhackme.com/r/p/paulemacedo">
-                                <SiTryhackme />
-                            </motion.a>
-
-                        </div>
-
-                    </motion.div>
+                    <motion.a whileHover={{ scale: 1.2 }} href="https://tryhackme.com/r/p/paulemacedo">
+                        <SiTryhackme />
+                    </motion.a>
+                    </div>
+                </motion.div>
                 </motion.div>
 
                 <motion.img
-                    src={profilepic}
-                    initial={{ opacity: 0, scale: 0.8 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    viewport={{ once: true }}
-                    transition={{ duration: 1}}
-                    className="w-[300px] md:w-[450px]"
+                src={profilepic}
+                initial={{ opacity: 0, scale: 0.8 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1}}
+                className="w-[300px] md:w-[450px]"
                 />
             </div>
 
