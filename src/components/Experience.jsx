1570ccad4eebd6a821f773bf728c788191b7798e
Paule Macedo <paulemacedo@outlook.com>
2025-02-08 03:10:52 -0300

    feat: enhance Experience component with detailed descriptions and responsive layout adjustments

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,16 +10,16 @@ const Experience = () => {
     {
       company: t('experience.experiences.0.company'),
       role: t('experience.experiences.0.role'),
-      description: t('experience.experiences.0.description'),
+      description: t('experience.experiences.0.description', { returnObjects: true }),
       duration: t('experience.experiences.0.duration'),
     }
   ];
 
   return (
-    <div className='p-8 max-w-[800px] mx-auto'>
-      <h1 className='text-4xl text-gray-200 font-bold text-center mb-12' id='experience'>{t('experience.title')}</h1>
+    <div className='p-4 sm:p-8 max-w-full sm:max-w-[800px] mx-auto'>
+      <h1 className='text-2xl sm:text-4xl text-gray-200 font-bold text-center mb-8 sm:mb-12' id='experience'>{t('experience.title')}</h1>
       <motion.div
-        className='space-y-8 flex flex-col items-center'
+        className='space-y-4 sm:space-y-8 flex flex-col items-center'
         initial="hidden"
         animate="visible"
       >
@@ -29,12 +29,24 @@ const Experience = () => {
               initial="hidden"
               whileInView="visible"
               transition={{ duration: 1 }}
-              className='border border-purple-600 p-6 rounded-lg shadow-md hover:shadow-xl transition duration-300 bg-purple-700/10'
+              className='border border-purple-600 p-4 sm:p-6 rounded-lg shadow-md hover:shadow-xl transition duration-300 bg-purple-700/10 w-full'
             >
-              <h2 className='text-gray-100 text-2xl font-semibold'>{experience.company}</h2>
-              <h4 className='text-gray-200'>{experience.role}</h4>
-              <p className='text-gray-300'>{experience.duration}</p>
-              <p className='text-gray-300'>{experience.description}</p>
+              <div className='justify-between flex items-center'>
+                <h2 className='text-gray-100 text-xl sm:text-2xl font-semibold'>{experience.company}</h2>
+                <p className='text-gray-300 text-sm sm:text-base'>{experience.duration}</p>
+              </div>
+              <h4 className='text-gray-200 mb-2 sm:mb-4 text-base md:text-lg'>{experience.role}</h4>
+              {Array.isArray(experience.description) ? (
+                <ul className='list-disc list-inside text-gray-300 text-base md:text-lg'>
+                {experience.description.map((desc, i) => (
+                  <li key={i} dangerouslySetInnerHTML={{ __html: desc }} className='mb-2'>
+
+                  </li>
+                ))}
+              </ul>
+              ) : (
+                <p className='text-gray-300 text-base md:text-base'>{experience.description}</p>
+              )}       
             </motion.div>
           </Reveal>
         ))}
