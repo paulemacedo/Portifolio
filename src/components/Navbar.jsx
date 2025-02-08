975b601667a978ff8fc922bd8f212f289a3aefdd
Paule Macedo <paulemacedo@outlook.com>
2025-02-08 00:22:17 -0300

    fix: adjust Navbar component height for improved layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,8 +46,8 @@ const Navbar = () => {
  };
 
  return (
-  <div className='fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md z-50 py-4'>
-     <div className='max-w-[1300px] mx-auto flex justify-between text-gray-200 text-xl items-center px-12 h-20'>
+  <div className='fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md z-10 py-4'>
+     <div className='max-w-[1300px] mx-auto flex justify-between text-gray-200 text-xl items-center px-12 h-10'>
        <a href='#'>
          <img src={Logo} alt="logo" className='w-14 h-14' />
        </a>
